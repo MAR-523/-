3345664bff043aba783122be628ae679a09bdddd
MAR-523 <82248847+MAR-523@users.noreply.github.com>
2022-07-29 16:25:57 +0800

    Update sm3.h

--- sm3_optimize/sm3.h
+++ sm3_optimize/sm3.h
@@ -4,9 +4,14 @@
 
 #include <cstdint>
 #include <vector>
+#include <immintrin.h>
+#include <xmmintrin.h>
 
 typedef uint32_t Word;
 typedef uint8_t Byte;
+typedef __m256i Word_8;
+
+static Word state[8];
 
 class SM3 {
 private:
@@ -20,5 +25,16 @@ public:
 	static Word* hash(Byte* input, uint64_t inputLen);
 };
 
+class SM3_SIMD {
+private:
+	static void CF_SIMD(Word* V, Byte* B);
+	static Word P0_SIMD(Word X);
+	static Word P1_SIMD(Word X);
+	static Word FF_SIMD(int j, Word X, Word Y, Word Z);
+	static Word GG_SIMD(int j, Word X, Word Y, Word Z);
+	static Word T_SIMD(int j);
+public:
+	static Word* hash_SIMD(Byte* input, uint64_t inputLen);
+};
 
 #endif //SM3_SM3_H
